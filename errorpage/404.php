e82f32ad85886c9b95476dce4e4d16c005d2b4c4
Nikolai Plath <der.el.kuku@gmail.com>
2017-04-14 23:20:54 -0500

    Formatting

--- errorpage/404.php
+++ errorpage/404.php
@@ -1,19 +1,22 @@
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
 <html>
 <head>
-<title>404 Not Found ;(</title>
+    <title>404 Not Found ;(</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
 <h1>Not Found <code>;(</code></h1>
 <p>
-The requested URL <code style="color: green; font-size: 1.5em"><?= $_SERVER['REQUEST_URI'] ?></code> was not found on this bloody server.
+    The requested URL <code style="color: green; font-size: 1.5em"><?= $_SERVER['REQUEST_URI'] ?></code> was not found
+    on this bloody server.
 </p>
-<hr />
+<hr/>
 <code>
-Freakin' <?= apache_get_version() ?> Server at <?= $_SERVER['SERVER_NAME'] ?> Port <?= $_SERVER['SERVER_PORT'] ?> says:
+    Freakin' <?= apache_get_version() ?> Server at <?= $_SERVER['SERVER_NAME'] ?> Port <?= $_SERVER['SERVER_PORT'] ?>
+    says:
 </code>
 <h1 style="text-align: center; color: red">&dagger; 404&hellip;</h1>
 <code><?= date('Y-m-d H:i:s') ?> in <?= date_default_timezone_get() ?></code>
-<hr />
+<hr/>
 </body>
 </html>
